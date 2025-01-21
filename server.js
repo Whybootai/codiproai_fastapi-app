2e9b91ebb198ab784e30377d13951c7dff7e2902
Whybootai <info.editorush@gmail.com>
2025-01-21 13:40:30 -0500

    Create server.js
    created backend

---
+++ server.js
@@ -0,0 +1,15 @@
+{
+  "name": "lightning-leads-backend",
+  "version": "1.0.0",
+  "description": "Backend for the Lightning Leads app",
+  "main": "server.js",
+    "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+    "dependencies": {
+      "cors": "^2.8.5",
+      "express": "^4.18.2",
+      "nodemon": "^3.0.1"
+    }
+}
